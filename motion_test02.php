78dd0a5e1d56947af30f97eb830225918fb7c066
yukisnow0704 <yukisnow0704@gmail.com>
2016-04-14 17:56:04 +0900

    weather news new!!

---
+++ motion_test02.php
@@ -0,0 +1,36 @@
+<?php
+	$url = "http://api.openweathermap.org/data/2.5/weather";
+	$ariadates = array();
+
+	#取得したいエリアの配列
+	$arias = array(
+		"Hamamatsu",
+		"Fukuroi",
+		"Shimizu"
+	);
+
+	#エリアごとの記録
+	foreach ($arias as $aria) {
+		$ariaurl = $url;
+		$ariaurl .= "?q=";
+		$ariaurl .= $aria;
+		$ariaurl .= ",jp";
+
+		$json = file_get_contents($ariaurl);
+		$json = mb_convert_encoding($json, 'utf8', 'ASCII,JIS,,UTF-8,EUC-JP,SJIS-WIN');
+		$datas = json_decode($json, true);
+
+		$ariadates[$aria]['temp'] = $datas['main']['temp']-273.15;
+		$ariadates[$aria]['temp_max'] = $datas['main']['temp_max']-273.15;
+		$ariadates[$aria]['temp_min'] = $datas['main']['temp_min']-273.15;
+		$ariadates[$aria]['weather'] = $datas['weather'][0]['main'];
+		$ariadates[$aria]['humidity'] = $datas['main']['humidity'];
+		$ariadates[$aria]['wind_speed'] = $datas['wind']['speed'];
+	}
+
+	exec("amixer cset numid=3 1");
+	
+	foreach ($arias as $aria) {
+		$talkdate = $aria;
+		exec("/home/pi/aquestalkpi/AquesTalkPi '".$talkdata."' | aplay");
+	};
