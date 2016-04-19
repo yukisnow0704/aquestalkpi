6b96a5f84221fbdaf52b920085130a6a46844945
yukisnow0704 <yukisnow0704@gmail.com>
2016-04-19 12:20:41 +0900

    no message

--- debu.php
+++ debu.php
@@ -3,5 +3,6 @@
 	exec("/home/pi/aquestalkpi/AquesTalkPi -s 300 '".$talkdate."' | aplay");
 	for ($i=0; $i < 100 ; $i++) { 
 		$talkdate = "デブ、";
+		echo $talkdate;
 		exec("/home/pi/aquestalkpi/AquesTalkPi -s 300 '".$talkdate."' | aplay");
 	}
--- motion_test04.php
+++ motion_test04.php
@@ -1,5 +1,9 @@
 <?php
-	$url = "https://ajax.googleapis.com/ajax/services/feed/load?v=1.0&q=https://goo.gl/kceVjB";
+	
+	define('WP_MEMORY_LIMIT', '64M');
+	$url = "https://ajax.googleapis.com/ajax/services/feed/load?v=1.0&q=";
+	$url .= "https://goo.gl/6SdYiQ";
+	# https://goo.gl/o2IFFc テクノロジー関係
 
 	$context = stream_context_create(
   	array(
@@ -9,8 +13,21 @@
     	)
   	));
 
-	#エリアごとの記録
-	$json = file_get_contents($url, false);#, $context);
+  	$news = array();
+
+	$json = file_get_contents($url, false, $context);
 	$json = mb_convert_encoding($json, 'utf8', 'ASCII,JIS,,UTF-8,EUC-JP,SJIS-WIN');
 	$dates = json_decode($json, true);
-	print_r($dates);
+
+	$talkdate = "最新のニュースをお伝えします。";
+
+	for ($i=0; $i <= 3; $i++) {
+		$news[$i] = $dates["responseData"]["feed"]["entries"][$i]["title"];
+		$talkdate .= $news[$i];
+		$talkdate .= "。";
+	}
+
+	$talkdate .= "以上です。";
+
+	echo $talkdate;
+	exec("/home/pi/aquestalkpi/AquesTalkPi '".$talkdate."' | aplay");
