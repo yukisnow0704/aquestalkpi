9cb408f5065c7b1f4a6a67adcf7bff71fe0094cc
yukisnow0704 <yukisnow0704@gmail.com>
2016-04-20 22:30:26 +0900

    no message

--- motion_test06.py
+++ motion_test06.py
@@ -1,29 +1,33 @@
 def reading(sensor):
-	import time
-	import RPi.GPIO as GPIO
-	GPIO.setwarings(False)
-
-	GPIO.setmode(GPIO.BOARD)
-	TRIG=8
-	ECHO=7
-
-	if sensor == 0:
-		GPIO.setup(TRIG, GPIO.OUT)
-		GPIO.setup(TRIG, GPIO.IN)
-		GPIO.output(TRIG, GPIO.LOW)
-		time.sleep(0.3)
-
-		GPIO.output(TRIG, True)
-		time.sleep(0.00001)
-		GPIO.output(TRIG, False)
-		while GPIO.input(ECHO)	:
-			signaloff = time.time()
-
-		timespassed = signalon - signaloff
-		distance = timespassed * 17000
-		return distance
-		GPIO.cleanup()
-	else:
-		print "Incorrect usonic () function varible."
-
-	print readign(0)
+    import time
+    import RPi.GPIO as GPIO
+    GPIO.setwarnings(False)
+     
+    GPIO.setmode(GPIO.BOARD)
+    TRIG = 11
+    ECHO = 13
+     
+    if sensor == 0:
+        GPIO.setup(TRIG,GPIO.OUT)
+        GPIO.setup(ECHO,GPIO.IN)
+        GPIO.output(TRIG, GPIO.LOW)
+        time.sleep(0.3)
+         
+        GPIO.output(TRIG, True)
+        time.sleep(0.00001)
+        GPIO.output(TRIG, False)
+ 
+        while GPIO.input(ECHO) == 0:
+          signaloff = time.time()
+         
+        while GPIO.input(ECHO) == 1:
+          signalon = time.time()
+ 
+        timepassed = signalon - signaloff
+        distance = timepassed * 17000
+        return distance
+        GPIO.cleanup()
+    else:
+        print "Incorrect usonic() function varible."
+         
+print reading(0)
