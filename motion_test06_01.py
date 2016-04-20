71ac4504ce62cddfd0ba13f35e0484efcce2d2eb
yukisnow0704 <yukisnow0704@gmail.com>
2016-04-21 02:51:51 +0900

    no message

--- motion_test06_01.py
+++ motion_test06_01.py
@@ -1,5 +1,3 @@
-from time import sleep
-
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(18, GPIO.IN)
 
@@ -9,8 +7,6 @@ try:
                 if GPIO.input(18)==1:
 	                os.system("/home/pi/source/aquestalkpi/motion_test02.sh")
 	            	
-                sleep(1)
-
 except KeyboardInterrupt:
         pass
 
