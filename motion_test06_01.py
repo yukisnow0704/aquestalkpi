3c591515c7783da11381b14bfbfe2e32764d9261
yukisnow0704 <yukisnow0704@gmail.com>
2016-04-21 02:50:46 +0900

    no message

--- motion_test06_01.py
+++ motion_test06_01.py
@@ -8,7 +8,9 @@ try:
                 print GPIO.input(18)
                 if GPIO.input(18)==1:
 	                os.system("/home/pi/source/aquestalkpi/motion_test02.sh")
-	            sleep(1)
+	            	
+                sleep(1)
+
 except KeyboardInterrupt:
         pass
 
