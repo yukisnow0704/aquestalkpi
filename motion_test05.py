36d2025af5460a60414f3bd5b97d0bce6c403a5f
yukisnow0704 <yukisnow0704@gmail.com>
2016-04-20 21:05:09 +0900

    no message

--- motion_test05.py
+++ motion_test05.py
@@ -8,7 +8,9 @@ GPIO.setup(18, GPIO.IN)
 try:
         while True:        
                 print GPIO.input(18)
-                os.system("/home/pi/source/aquestalkpi/motion_test02.sh")
+                if GPIO.input(18)==1:
+	                os.system("/home/pi/source/aquestalkpi/motion_test02.sh")
+	                
                 sleep(1)
 
 except KeyboardInterrupt:
