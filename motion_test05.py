216ecd4afb1b55dc7d0553fd24af63bb693dd5a2
yukisnow0704 <yukisnow0704@gmail.com>
2016-04-20 20:42:15 +0900

    no message

---
+++ motion_test05.py
@@ -0,0 +1,17 @@
+import os
+import RPi.GPIO as GPIO
+from time import sleep
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(18, GPIO.IN)
+
+try:
+        while True:        
+                print GPIO.input(18)
+                os.system("/home/pi/source/aquestalkpi/motion_test02.sh")
+                sleep(1)
+
+except KeyboardInterrupt:
+        pass
+
+GPIO.cleanup()
