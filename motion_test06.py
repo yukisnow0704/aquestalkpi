7a8e2caab251f2b18676239b90db7e50eccde4ed
yukisnow0704 <yukisnow0704@gmail.com>
2016-04-21 02:35:02 +0900

    no message

--- motion_test06.py
+++ motion_test06.py
@@ -84,12 +84,11 @@ def reading(sensor):
 			GPIO.setup(18, GPIO.IN)
 
 			try:
-		        while True:        
-        	        print GPIO.input(18)
-            	    if GPIO.input(18)==1:
-	            	    os.system("/home/pi/source/aquestalkpi/motion_test02.sh")
-	            	
-                sleep(1)
+				while True:
+					print GPIO.input(18)
+					if GPIO.input(18)==1:
+						os.system("/home/pi/source/aquestalkpi/motion_test02.sh")
+						sleep(1)
 
 		return distance
 		
