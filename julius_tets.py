61e2a566905ec4add8375a3106346c01a9cb559b
yukisnow0704 <yukisnow0704@gmail.com>
2016-04-25 18:55:20 +0900

    no message

--- julius_tets.py
+++ julius_tets.py
@@ -9,14 +9,15 @@ def main():
     bufsize = 4096
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect((host,port))
+
     while True:
         recv_data = sock.recv(bufsize)
         print (recv_data)
         if recv_data.find('WHYPO') != -1:
             print (recv_data)
-            if recv_data.find(u'天気') != -1:
+            if recv_data.find('天気') != -1:
                 print("天気")
-            if recv_data.find(u'ニュース') != -1:
+            if recv_data.find('ニュース') != -1:
                 print("ニュース")
 if __name__ == '__main__':
     main()
