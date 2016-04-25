121989abf8807e1a12f731b1ead9a4f31e82ebd2
yukisnow0704 <yukisnow0704@gmail.com>
2016-04-25 19:24:37 +0900

    no message

---
+++ julius_test_01.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import socket
+import requests
+import re
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(('localhost', 10500))
+
+sf = s.makefile('')
+
+reWATSON = re.compile(r'WHYPO WORD="WATSON" .* CM="(\d\.\d*)"')
+
+while True:
+    line = sf.readline().decode('utf-8')
+    #if line.find('WHYPO') != -1:
+    tmp = reWATSON.search( line )
+    if tmp:
+        print line
+        if float(tmp.group(1)) > 0.8:
+                print 'call WATSON'
