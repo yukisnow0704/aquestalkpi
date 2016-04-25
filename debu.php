b6772e2f938aba44c86c14645d777f394950bb40
yukisnow0704 <yukisnow0704@gmail.com>
2016-04-25 14:22:01 +0900

    no message

--- debu.php
+++ debu.php
@@ -2,7 +2,7 @@
 	$talkdate = "長澤くんは、";
 	exec("/home/pi/aquestalkpi/AquesTalkPi -s 300 '".$talkdate."' | aplay");
 	for ($i=0; $i < 100 ; $i++) { 
-		if ($i%10=0){
+		if ($i%10==0){
 			$talkdate = "長澤くんは、";
 			exec("/home/pi/aquestalkpi/AquesTalkPi '".$talkdate."' | aplay");
 		}
