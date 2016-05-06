6d3d7c58e1ee9780b3cc8d88b33c5baf78cad43a
yukisnow0704 <yukisnow0704@gmail.com>
2016-05-06 15:48:34 +0900

    no message

--- julius_test01.py
+++ julius_test01.py
@@ -16,11 +16,3 @@ while True:
 	line = sf.readline().decode('utf-8')
 	if line.find('WHYPO') != -1:
 		print line
-		if line.find(u"天気予報") != -1:
-			print 'call tenki'
-			os.system("pause /n")
-			os.system("/home/pi/source/aquestalkpi/motion_test02.sh")
-		if line.find(u"ニュース") != -1:
-			print 'call news'
-			os.system("/home/pi/source/aquestalkpi/motion_test04.sh")
-			time.sleep(5)
