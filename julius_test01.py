8038a60b61745efafec1060228c430338cf61e50
yukisnow0704 <yukisnow0704@gmail.com>
2016-05-18 19:24:09 +0900

    no message

--- julius_test01.py
+++ julius_test01.py
@@ -8,7 +8,7 @@ julius_path = 'julius'
 jconf_path = '~/julius-4.3.1/julius-kits/dictation-kit-v4.3.1-linux/kudo_ken.jconf'
 args = julius_path + ' -C ' + jconf_path + ' -module '
 p = subprocess.Popen(args, stdout=subprocess.PIPE, shell=True)
-pid = p.stdout.read() # juliusのプロセスIDを取得
+pid = p.stdout.read()
 time.sleep(5.0)
-p.kill() # 起動スクリプトのプロセスを終了
-subprocess.call(["kill " + pid], shell=True) # juliusのプロセスを終了
+p.kill()
+subprocess.call(["kill " + pid], shell=True)
