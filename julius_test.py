90cc886551c61440adf092ade0fa4b9c3e48f127
yukisnow0704 <yukisnow0704@gmail.com>
2016-05-18 18:59:50 +0900

    no message

--- julius_test.py
+++ julius_test.py
@@ -18,7 +18,12 @@ julius_socket = None
 def invoke_julius():
     print 'INFO : invoke julius'
     args = julius_path + ' -C ' + jconf_path + ' -module '
-    p = subprocess.Popen( shlex.split(args) )
+    p = subprocess.Popen(
+            shlex.split(args),
+            stdin=None,
+            stdout=None,
+            stderr=None
+        )
     print 'INFO : invoke julius complete.'
     print 'INFO : wait 2 seconds.'
     time.sleep(3.0)
