3a87f4660e564dc3ea52c712c1c93b4bf380a7f5
yukisnow0704 <yukisnow0704@gmail.com>
2016-05-18 18:16:30 +0900

    no message

--- julius_test01.py
+++ julius_test01.py
@@ -8,4 +8,5 @@ args = julius_path + ' -C ' + jconf_path + ' '
 proc1 = subprocess.Popen(args, shell=True)
 proc2 = proc1.stdout.read()
 time.sleep(5.0)
+proc1.kill()
 subprocess.call(["kill " + proc2], shell=True)
