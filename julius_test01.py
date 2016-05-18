0587f5cbf6cc18c81c9af57cda7be46b92868e84
yukisnow0704 <yukisnow0704@gmail.com>
2016-05-18 19:31:44 +0900

    no message

--- julius_test01.py
+++ julius_test01.py
@@ -7,7 +7,7 @@ from subprocess import Popen
 
 julius_path = 'julius'
 jconf_path = '~/julius-4.3.1/julius-kits/dictation-kit-v4.3.1-linux/kudo_ken.jconf'
-args = julius_path + ' -C ' + jconf_path + ' -module '
+args = julius_path + ' -C ' + jconf_path + ''
 p = Popen(args, shell=True)
 time.sleep(5.0)
 p.terminate()
