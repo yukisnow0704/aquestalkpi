fbaedd698be697d968fa12fc97bce5bf0a4acf19
yukisnow0704 <yukisnow0704@gmail.com>
2016-05-18 19:17:08 +0900

    no message

--- julius_test01.py
+++ julius_test01.py
@@ -7,6 +7,6 @@ julius_path = 'julius'
 jconf_path = '~/julius-4.3.1/julius-kits/dictation-kit-v4.3.1-linux/kudo_ken.jconf'
 args = julius_path + ' -C ' + jconf_path + ' -module '
 print args
-proc1 = subprocess.Popen(shlex.split(args))
+proc1 = subprocess.Popen(args)
 time.sleep(5.0)
 proc1.kill()
