947ae8e41ea7564a18375560b19167f67c971458
yukisnow0704 <yukisnow0704@gmail.com>
2016-05-18 18:06:07 +0900

    no message

--- julius_test01.py
+++ julius_test01.py
@@ -7,4 +7,4 @@ jconf_path = '~/julius-4.3.1/julius-kits/dictation-kit-v4.3.1-linux/kudo_ken.jco
 args = julius_path + ' -C ' + jconf_path + ' '
 proc1 = subprocess.Popen(args, shell=True)
 time.sleep(5.0)
-os.kill(proc1.pid, signal.SIGTERM)
+proc1.terminate()
