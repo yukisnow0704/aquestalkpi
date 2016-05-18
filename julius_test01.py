1b7dc455eede37347e605b95af83116d3fa88aff
yukisnow0704 <yukisnow0704@gmail.com>
2016-05-18 19:31:00 +0900

    no message

--- julius_test01.py
+++ julius_test01.py
@@ -3,12 +3,11 @@ import time
 import os
 import signal
 import shlex
+from subprocess import Popen
 
 julius_path = 'julius'
 jconf_path = '~/julius-4.3.1/julius-kits/dictation-kit-v4.3.1-linux/kudo_ken.jconf'
 args = julius_path + ' -C ' + jconf_path + ' -module '
-p = subprocess.Popen(args, stdout=subprocess.PIPE, shell=True)
-pid = p.stdout.read()
+p = Popen(args, shell=True)
 time.sleep(5.0)
-p.kill()
-subprocess.call(["kill " + pid], shell=True)
+p.terminate()
