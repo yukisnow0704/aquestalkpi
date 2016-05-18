d9c8b69e691484e58d57e8143d4a8d84d2fcbef3
yukisnow0704 <yukisnow0704@gmail.com>
2016-05-18 17:41:13 +0900

    no message

--- julius_test01.py
+++ julius_test01.py
@@ -3,6 +3,6 @@ import time
 julius_path = 'julius'
 jconf_path = '~/julius-4.3.1/julius-kits/dictation-kit-v4.3.1-linux/kudo_ken.jconf'
 args = julius_path + ' -C ' + jconf_path + ' -module '
-pid = subprocess.Popen(args)
+pid = subprocess.Popen(args, shell=True)
 time.sleep(5.0)
 subprocess.kill(pid)
