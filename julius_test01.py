8032d4b91e752fe4a9224664f32c29d9e6e4a66d
yukisnow0704 <yukisnow0704@gmail.com>
2016-05-18 17:48:50 +0900

    no message

--- julius_test01.py
+++ julius_test01.py
@@ -1,8 +1,9 @@
 import subprocess
 import time
+import os
 julius_path = 'julius'
 jconf_path = '~/julius-4.3.1/julius-kits/dictation-kit-v4.3.1-linux/kudo_ken.jconf'
 args = julius_path + ' -C ' + jconf_path + ' -module '
-pid = subprocess.Popen(args, shell=True)
+proc1 = subprocess.Popen(args, shell=True)
 time.sleep(5.0)
-subprocess.kill(pid)
+os.kill(proc1.pid, signal.SIGTERM)
