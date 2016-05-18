dc4fd92299671efcd56acd28c0d01500b8d2d475
yukisnow0704 <yukisnow0704@gmail.com>
2016-05-18 18:47:43 +0900

    no message

--- julius_test01.py
+++ julius_test01.py
@@ -6,7 +6,6 @@ import shlex
 julius_path = 'julius'
 jconf_path = '~/julius-4.3.1/julius-kits/dictation-kit-v4.3.1-linux/kudo_ken.jconf'
 args = julius_path + ' -C ' + jconf_path + ' '
-proc1 = subprocess.Popen(shlex.split(args)).pid
+proc1 = subprocess.Popen(shlex.split(args))
 time.sleep(5.0)
-print proc1
-subprocess.call(["kill " + str(proc1)], shell=True)
+proc1.kill()
