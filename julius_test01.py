fe12433a651ec25b5098233b7aab9c6a33f3739b
yukisnow0704 <yukisnow0704@gmail.com>
2016-05-18 17:49:25 +0900

    no message

--- julius_test01.py
+++ julius_test01.py
@@ -1,6 +1,7 @@
 import subprocess
 import time
 import os
+import signal
 julius_path = 'julius'
 jconf_path = '~/julius-4.3.1/julius-kits/dictation-kit-v4.3.1-linux/kudo_ken.jconf'
 args = julius_path + ' -C ' + jconf_path + ' -module '
