77dfcd9a336aadca5a0a34226f02b70cfc44d9fb
yukisnow0704 <yukisnow0704@gmail.com>
2016-07-25 11:13:27 +0900

    no message

--- julius_demo05/julius_test.py
+++ julius_demo05/julius_test.py
@@ -37,8 +37,8 @@ def stop(tmp):
 				print 'ok!stop'
 
 #ジュリアス起動
-julius_path = 'julius'
-jconf_path = '~/julius-4.3.1/julius-kits/dictation-kit-v4.3.1-linux/kudo_ken.jconf'
+julius_path = '~/julius-4.3.1/julius/julius'
+jconf_path = '~/julius-kits/dictation-kit-v4.3.1-linux/kudo_ken.dic'
 julius = None
 julius_socket = None
 
