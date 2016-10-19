68ba7b47e63d66511ec117f8dfe5c57b547b0e91
yukisnow0704 <yukisnow0704@gmail.com>
2016-10-19 11:47:15 +0900

    no message

--- all_senser.php
+++ all_senser.php
@@ -1,8 +1,9 @@
 <?php
 
-$firepin=23;
-$gaspin=24;
+$firepin=11;
+$gaspin=19;
 $soundpin=8;
+$touchpin=26;
 
 $isFunction=false;
 
@@ -14,33 +15,40 @@ wiringPiSetupGpio();
 pinMode($firepin, 0);
 pinMode($gaspin, 0);
 pinMode($soundpin, 0);
+pinMode($touchpin, 0);
 
 
 while (true) {
-	while ($isFunction == false) {
-		$var = digitalRead($firepin);
-		if($var == 1){
-			$text = 'fire ';
-			$isFunction = true;
-		}
-		$var = digitalRead($gaspin);
-		if($var == 1){
-			$text .= 'gas ';
-			$isFunction = true;
-		}
-		$var = digitalRead($soundpin);
-		if($var == 0){
-			$text = 'sound ';
-			$isFunction = true;
-		}
-	}
+        while ($isFunction == false) {
+                $var = digitalRead($firepin);
+                if($var == 1){
+                        $text = 'fire,';
+                        $isFunction = true;
+                }
+                $var = digitalRead($gaspin);
+                if($var == 0){
+                        $text .= 'gas,';
+                        $isFunction = true;
+                }
+                $var = digitalRead($soundpin);
+                if($var == 0){
+                        $text = 'sound,';
+                        $isFunction = true;
+                }
+                $var = digitalRead($touchpin);
+                if($var == 1){
+                        $text .= 'touch,';
+                        $isFunction = true;
+                }
+        }
 
-	if ($isFunction) {
-		$messagePath = "/home/pi/aquestalkpi/AquesTalkPi " . $text . " | aplay -D plughw:2,0";
-        exec($messagePath);
+        if ($isFunction) {
+                $messagePath = "/home/pi/aquestalkpi/AquesTalkPi " . $text . " | aplay -D plughw:2,0";
+                exec($messagePath);
 
-        $text = '';
-        $isFunction = false;
-	}
+                $text = '';
+                $isFunction = false;
+        }
 
 }
+
---
+++ lab_dangar.php
@@ -0,0 +1,67 @@
+<?php
+
+$firepin=11;
+$gaspin=19;
+$soundpin=8;
+$touchpin=26;
+
+$isFunction=false;
+
+$text='';
+$var;
+$time = 0;
+
+wiringPiSetupGpio();
+
+pinMode($firepin, 0);
+pinMode($gaspin, 0);
+pinMode($soundpin, 0);
+pinMode($touchpin, 0);
+
+
+while (true) {
+        while ($isFunction == false) {
+                if(digitalRead($firepin) == 1){
+                        sleep(1);
+                        if(digitalRead($firepin) == 1){
+                                $text .= '火が出ているようです。';
+                                $isFunction = true;
+                        }
+                }
+
+                if(digitalRead($gaspin) == 0){
+                        sleep(1);
+                        if(digitalRead($gaspin) == 1){
+                                $text .= '可燃性のガスが発生しているようです。';
+                                $isFunction = true;
+                        }
+                }
+
+                while (digitalRead($touchpin) == 1) {
+                        exec("/home/pi/aquestalkpi/AquesTalkPi 20秒間サウンドセンサーが待機します。基本的には静かにお願いします。 | aplay -D plughw:2,0");        
+                        while ($time <= 20) {
+                                if(digitalRead($soundpin) == 0){
+                                        $text = 'サウンドセンサーが稼動しました。危険を感知しています。';
+                                        $isFunction = true;
+                                        $time = 20;
+                                }
+                                sleep(1);
+                                $time += 1;
+                        }
+                        if($isFunction == false && $time == 20)
+                        exec("/home/pi/aquestalkpi/AquesTalkPi サウンドセンサーの起動を停止します。 | aplay -D plughw:2,0");        
+
+                }
+
+
+        }
+
+        if ($isFunction) {
+                $messagePath = "/home/pi/aquestalkpi/AquesTalkPi " . $text . " | aplay -D plughw:2,0";
+                exec($messagePath);
+
+                $text = '';
+                $isFunction = false;
+        }
+
+}
