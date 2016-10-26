f7151a327c65bcd1af4107d29a5bb10448e71ec6
yukisnow0704 <yukisnow0704@gmail.com>
2016-10-26 21:42:43 +0900

    no message

---
+++ mongodb_test.php
@@ -0,0 +1,18 @@
+<?php
+
+// DBへ接続
+$mongo = new Mongo( "mongo://192.168.111.100:30000, 192.168.111.100:30001", array("replicaSet"=>true));
+
+// データベースを指定
+$db = $mongo->selectDB("sample1");
+
+// コレクションを指定1
+$col = $db->selectCollection("test1");
+
+// コレクションのドキュメントを全件取得
+$cursor = $col->find();
+
+// 表示
+foreach ($cursor as $id =>$obj) {
+	var_dump($obj);
+}
