aa5e33cd65d7cf7d810b13b904396a30a617ee29
yukisnow0704 <yukisnow0704@gmail.com>
2016-11-22 18:15:57 +0900

    no message

--- ras_mail_test/mail.php
+++ ras_mail_test/mail.php
@@ -13,7 +13,7 @@ $mail->SMTPDebug = 1; // debugging: 1 = errors and messages, 2 = messages only
 $mail->SMTPAuth = true;
 $mail->CharSet = 'utf-8';
 $mail->SMTPSecure = 'tls';
-$mail->Host = "smtp.gmail.com:465";
+$mail->Host = "64.233.188.108:465";
 $mail->IsHTML(false);
 $mail->Username = $smtp_user;
 $mail->Password = $smtp_password; 
