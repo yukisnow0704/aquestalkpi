a828c5aec85b6e790cb6b3fcf63aa80b29c18970
yukisnow0704 <yukisnow0704@gmail.com>
2016-11-25 19:42:37 +0900

    no message

--- ras_mail_test/mail.php
+++ ras_mail_test/mail.php
@@ -1,88 +1,34 @@
 <?php
-require_once __DIR__ . '/vendor/autoload.php';
+require_once ( 'PHPMailerAutoload.php' );
+$subject = "タイトル";
+$body = "メール本文";
+$fromname = "me";
+$from = "sist.kudolab@gmail.com";
+$smtp_user = "sist.kudolab@gmail.com";
+$smtp_password = "kudo0401";
 
+$mail = new PHPMailer();
+$mail->IsSMTP();
+$mail->SMTPDebug = 1; // debugging: 1 = errors and messages, 2 = messages only
+$mail->SMTPAuth = true;
+$mail->CharSet = 'utf-8';
+$mail->SMTPSecure = 'tls';
+$mail->Host = "smtp.gmail.com";
+$mail->Port = 587;
+$mail->IsHTML(false);
+$mail->Username = $smtp_user;
+$mail->Password = $smtp_password; 
+$mail->SetFrom($smtp_user);
+$mail->From     = $from;
+$mail->Subject = $subject;
+$mail->Body = $body;
+$mail->AddAddress('yukisnow0704@gmail.com');
 
-define('APPLICATION_NAME', 'Gmail API PHP Quickstart');
-define('CREDENTIALS_PATH', '~/.credentials/gmail-php-quickstart.json');
-define('CLIENT_SECRET_PATH', __DIR__ . '/client_secret.json');
-// If modifying these scopes, delete your previously saved credentials
-// at ~/.credentials/gmail-php-quickstart.json
-define('SCOPES', implode(' ', array(
-  Google_Service_Gmail::GMAIL_READONLY)
-));
-
-if (php_sapi_name() != 'cli') {
-  throw new Exception('This application must be run on the command line.');
-}
-
-/**
- * Returns an authorized API client.
- * @return Google_Client the authorized client object
- */
-function getClient() {
-  $client = new Google_Client();
-  $client->setApplicationName(APPLICATION_NAME);
-  $client->setScopes(SCOPES);
-  $client->setAuthConfig(CLIENT_SECRET_PATH);
-  $client->setAccessType('offline');
-
-  // Load previously authorized credentials from a file.
-  $credentialsPath = expandHomeDirectory(CREDENTIALS_PATH);
-  if (file_exists($credentialsPath)) {
-    $accessToken = json_decode(file_get_contents($credentialsPath), true);
-  } else {
-    // Request authorization from the user.
-    $authUrl = $client->createAuthUrl();
-    printf("Open the following link in your browser:\n%s\n", $authUrl);
-    print 'Enter verification code: ';
-    $authCode = trim(fgets(STDIN));
-
-    // Exchange authorization code for an access token.
-    $accessToken = $client->fetchAccessTokenWithAuthCode($authCode);
-
-    // Store the credentials to disk.
-    if(!file_exists(dirname($credentialsPath))) {
-      mkdir(dirname($credentialsPath), 0700, true);
-    }
-    file_put_contents($credentialsPath, json_encode($accessToken));
-    printf("Credentials saved to %s\n", $credentialsPath);
-  }
-  $client->setAccessToken($accessToken);
-
-  // Refresh the token if it's expired.
-  if ($client->isAccessTokenExpired()) {
-    $client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
-    file_put_contents($credentialsPath, json_encode($client->getAccessToken()));
-  }
-  return $client;
-}
-
-/**
- * Expands the home directory alias '~' to the full path.
- * @param string $path the path to expand.
- * @return string the expanded path.
- */
-function expandHomeDirectory($path) {
-  $homeDirectory = getenv('HOME');
-  if (empty($homeDirectory)) {
-    $homeDirectory = getenv('HOMEDRIVE') . getenv('HOMEPATH');
-  }
-  return str_replace('~', realpath($homeDirectory), $path);
-}
-
-// Get the API client and construct the service object.
-$client = getClient();
-$service = new Google_Service_Gmail($client);
-
-// Print the labels in the user's account.
-$user = 'me';
-$results = $service->users_labels->listUsersLabels($user);
-
-if (count($results->getLabels()) == 0) {
-  print "No labels found.\n";
+if( !$mail -> Send() ){
+    $message  = "Message was not sent<br/ >";
+    $message .= "Mailer Error: " . $mailer->ErrorInfo;
 } else {
-  print "Labels:\n";
-  foreach ($results->getLabels() as $label) {
-    printf("- %s\n", $label->getName());
-  }
+    $message  = "Message has been sent";
 }
+
+echo $message;
